f9106b1099b548776b26208cc1eedb179c23d418
Shota Yamamota <1230124fw@gmail.com>
2017-06-14 03:56:35 +0900

    first

---
+++ react-native.fish
@@ -0,0 +1,6 @@
+complete -f -c react-native -n '__fish_use_subcommand' -a run-android -d "build and run the android application"
+complete -f -c react-native -n '__fish_use_subcommand' -a run-ios -d "build and run the ios application"
+complete -f -c react-native -n '__fish_use_subcommand' -a init -d "generates a new project and installs its dependencies"
+
+complete -c react-native -s h -l help -d 'output usage information'
+complete -c react-native -s v -l version -d 'output the version number'
